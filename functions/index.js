b4863c913628320bc6716c4535447dcd54270685
Vikram Chowdary <vikramchowdary.parimi@gmail.com>
2018-04-30 16:11:46 +0530

    'resetUserDatabase' tests completed

--- functions/index.js
+++ functions/index.js
@@ -7,33 +7,12 @@ exports.helloWorld = functions.https.onRequest((req, res) => {
     res.send("Hello From Firebase");
 })
 
-exports.resetDBForUser = functions.https.onRequest((req, res) => {
+exports.setTextToChild = functions.https.onRequest((req, res) => {
     const queryText = req.query.text;
     admin.database().ref('/users').push({text: queryText}).then(snapshot => {
         res.redirect(303, snapshot.ref);
     })
 })
-
-exports.makeUpperCase = functions.database.ref('/users/{pushId}/text').onCreate((snapshot, context) => {
-    const original = snapshot.val();
-    console.log('Uppercasing', context.params.pushId, original);
-    const uppercaseText = original.toUpperCase();
-    return snapshot.ref.parent.child('uppercase ').set(uppercaseText);
-});     
-
-exports.resetUserDatabase = functions.https.onCall((data, context) => {
-    const userId = context.userId;
-    const ref = admin.database().ref('/Trip/'+ userId);
-    ref.once('value', function(snap) {
-        snap.forEach(function(childSnap) {
-            childSnap.ref.update({
-                'wasDeleted': true
-            });
-        })
-    }).then(() => {
-        return {text: 'success'}
-    })
-});
 
 var childNodes = ["Business",
     "CustomField",
@@ -42,28 +21,58 @@ var childNodes = ["Business",
     "Settings",
     "Trip",
     "Trip-Expenses",
-    "Typerate",
-    "Vehicle"]
+    "TypeRate",
+    "Vehicle"]    
 
-exports.testReset = functions.https.onRequest((req, res) => {
-    const userId = req.query.text;
-    var childNodesCount = childNodes.count;
+exports.resetUserDatabase = functions.https.onCall((data, context) => {
+    const userId = context.userId;
+    var childNodeIndex = childNodes.length
+    recursiveResetFunction(childNodes[childNodeIndex])
     function recursiveResetFunction(childNode) {
         const ref = admin.database().ref(childNode + '/' + userId);
         ref.once('value', function (snap) {
-            snap.forEach(function (childSnap) {
-                if (childNode != "CustomField" || childNode != "Settings") {
+            if (childNode == "CustomField" || childNode == "Settings") {
+                snap.ref.remove();
+            } else {
+                snap.forEach(function (childSnap) {
+                    console.log('childNode', childNode);
                     childSnap.ref.update({
                         'wasDeleted': true
                     });
-                } else {
-                    childSnap.ref.remove();
-                }
-            })
+                })
+            }
         }).then(snapshot => {
-            if (childNodesCount > 0) {
-                childNodesCount = childNodesCount - 1;
-                return recursiveResetFunction(childNodes[childNodesCount-1])
+            if (childNodeIndex > 0) {
+                childNodeIndex = childNodeIndex - 1;
+                return recursiveResetFunction(childNodes[childNodeIndex])
+            } else {
+                res.send('success');
+            }
+        });
+    }
+});
+
+exports.testReset = functions.https.onRequest((req, res) => {
+    const userId = req.query.text;
+    var childNodeIndex = childNodes.length
+    recursiveResetFunction(childNodes[childNodeIndex])
+    function recursiveResetFunction(childNode) {
+        const ref = admin.database().ref(childNode + '/' + userId);
+        ref.once('value', function (snap) {
+            if (childNode == "CustomField" || childNode == "Settings") {
+                snap.ref.remove();
+            } else {
+                snap.forEach(function (childSnap) {
+                    console.log('childNode', childNode);
+                    childSnap.ref.update({
+                        'wasDeleted': true
+                    });
+                })
+            }
+        }).then(snapshot => {
+            if (childNodeIndex > 0) {
+                childNodeIndex = childNodeIndex - 1;
+                return recursiveResetFunction(childNodes[childNodeIndex])
             } else {
                 res.send('success');
             }
