9e0096a3d7c1d44140db451b5baf3ca7b90f3c11
Vikram Chowdary <vikramchowdary.parimi@gmail.com>
2018-04-30 12:49:04 +0530

    Added recursive delete in test reset method

--- functions/index.js
+++ functions/index.js
@@ -22,28 +22,51 @@ exports.makeUpperCase = functions.database.ref('/users/{pushId}/text').onCreate(
 });     
 
 exports.resetUserDatabase = functions.https.onCall((data, context) => {
-    const userId = data.text;
+    const userId = context.userId;
     const ref = admin.database().ref('/Trip/'+ userId);
-    ref.once('value').then(snap => {
-        snap.forEach(element => {
-            element.ref.child('wasDeleted').set(true);
-        });
-    }).then(() => {
-        return {text: 'success'}
-    })
-});
-
-
-exports.testReset = functions.https.onRequest((req, res) => {
-    const userId = req.query.text;
-    const ref = admin.database().ref('/Business/'+ userId);
     ref.once('value', function(snap) {
         snap.forEach(function(childSnap) {
             childSnap.ref.update({
                 'wasDeleted': true
             });
         })
-    }).then(snapshot => {
-        res.send('success');
-    });
+    }).then(() => {
+        return {text: 'success'}
+    })
+});
+
+var childNodes = ["Business",
+    "CustomField",
+    "ExpenseCategory",
+    "FavoriteTrips",
+    "Settings",
+    "Trip",
+    "Trip-Expenses",
+    "Typerate",
+    "Vehicle"]
+
+exports.testReset = functions.https.onRequest((req, res) => {
+    const userId = req.query.text;
+    var childNodesCount = childNodes.count;
+    function recursiveResetFunction(childNode) {
+        const ref = admin.database().ref(childNode + '/' + userId);
+        ref.once('value', function (snap) {
+            snap.forEach(function (childSnap) {
+                if (childNode != "CustomField" || childNode != "Settings") {
+                    childSnap.ref.update({
+                        'wasDeleted': true
+                    });
+                } else {
+                    childSnap.ref.remove();
+                }
+            })
+        }).then(snapshot => {
+            if (childNodesCount > 0) {
+                childNodesCount = childNodesCount - 1;
+                return recursiveResetFunction(childNodes[childNodesCount-1])
+            } else {
+                res.send('success');
+            }
+        });
+    }
 });
